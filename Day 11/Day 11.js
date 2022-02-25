21f774ea2c708dd78e23090930e8771b0375c9bb
Rockky1997 <ramchakraborty9867@gmail.com>
2022-02-25 09:04:28 +0530

    Day 11 problem is added

---
+++ Day 11/Day 11.js
@@ -0,0 +1,31 @@
+const body = document.body;
+const br = document.createElement('br')
+
+let number1 = 10;
+
+if(number1%10 == 0){      
+
+  //do something;
+  document.write('number 10 is  divisible by 10');  
+
+}else{
+
+  //do otherwise;
+  document.write('number 10 is not divisible by 10');
+
+}
+body.append(br);
+
+let number2 = 17;
+
+if(number2%10 == 0){      
+
+  //do something;
+  document.write('number 7 is  divisible by 10');  
+
+}else{
+
+  //do otherwise;
+  document.write('number 7 is not divisible by 10');
+
+}
